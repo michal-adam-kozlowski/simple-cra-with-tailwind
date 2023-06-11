490a1ba46ad13856d3b5760ba69231ad93e2c7df
Michal Kozlowski <michalkozlowski@Michal-Kozlowski-Macbook-Pro.local>
2023-06-11 10:05:57 +0200

    init commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="p-4 m-5 bg-black rounded text-white border-4 border-red-300">
+      Hello World!
+    </div>
+  );
+}
+
+export default App;
